f1ee7e10bc61eddac8b853234d5528134f27f571
Orzzis <kirill72tyum@gmail.com>
2023-06-10 01:53:56 +0500

    1 задача

---
+++ Pr3/Pr3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите количество целых положительных числе:");
+int n = int.Parse(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        sum += i;
+    }
+}
+Console.WriteLine($"Сумма первых {n} целых положительных чисел равна {sum}")
